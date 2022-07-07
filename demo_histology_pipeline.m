34905afefcb85532da90918a7242725cdffedc57
Andrew Peters <peters.andrew.j@gmail.com>
2022-07-07 22:11:48 +0100

    typo in demo pipeline

--- demo_histology_pipeline.m
+++ demo_histology_pipeline.m
@@ -77,7 +77,7 @@ ccf_points_idx = sub2ind(size(av),ccf_points_cat(:,1),ccf_points_cat(:,2),ccf_po
 % Find annotated volume (AV) values at points
 ccf_points_av = av(ccf_points_idx);
 % Get areas from the structure tree (ST) at given AV values
-ccf_points_areas = st(ccf_points_areas,:).safe_name;
+ccf_points_areas = st(ccf_points_av,:).safe_name;
 
 
 
