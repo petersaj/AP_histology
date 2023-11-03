6b691ab70689c45c7a483338dc11ec92d031885a
Andy Peters <peters.andrew.j@gmail.com>
2023-11-03 15:22:31 +0000

    update

--- AP_histology.m
+++ AP_histology.m
@@ -3,8 +3,8 @@ function AP_histology
 
 % Set up the gui
 screen_size_px = get(0,'screensize');
-gui_aspect_ratio = 5; % width/length
-gui_width_fraction = 0.5; % fraction of screen width to occupy
+gui_aspect_ratio = 2; % width/length
+gui_width_fraction = 0.3; % fraction of screen width to occupy
 gui_border = 50; % border from gui to screen edge
 gui_width_px = screen_size_px(3).*gui_width_fraction;
 gui_height_px = gui_width_px/gui_aspect_ratio;
@@ -18,41 +18,41 @@ histology_toolbar_gui = figure('Toolbar','none','Menubar','none','color','w', ..
     'Units','pixels','Position',gui_position);
 
 % Set up the text to display coordinates
-gui_data.gui_text = annotation('textbox','String','','interpreter','none', ...
+gui_data.gui_text = annotation('textbox','String','','interpreter','tex', ...
     'Units','normalized','Position',[0,0,1,1],'VerticalAlignment','top', ...
     'FontSize',12,'FontName','Consolas','PickableParts','none');
 
 % File menu
-file_menu = uimenu(histology_toolbar_gui,'Text','File selection');
-uimenu(file_menu,'Text','Set raw image path','MenuSelectedFcn',{@set_image_path,histology_toolbar_gui});
-uimenu(file_menu,'Text','Set processing save path','MenuSelectedFcn',{@set_save_path,histology_toolbar_gui});
+gui_data.menu.file = uimenu(histology_toolbar_gui,'Text','File selection');
+uimenu(gui_data.menu.file,'Text','Set raw image path','MenuSelectedFcn',{@set_image_path,histology_toolbar_gui});
+uimenu(gui_data.menu.file,'Text','Set processing save path','MenuSelectedFcn',{@set_save_path,histology_toolbar_gui});
 
 % Preprocessing menu
-preprocess_menu = uimenu(histology_toolbar_gui,'Text','Image preprocessing');
-uimenu(preprocess_menu,'Text','Create slice images','MenuSelectedFcn', ...
+gui_data.menu.preprocess = uimenu(histology_toolbar_gui,'Text','Image preprocessing');
+uimenu(gui_data.menu.preprocess,'Text','Create slice images','MenuSelectedFcn', ...
     {@ap_histology.create_slice_images,histology_toolbar_gui});
-uimenu(preprocess_menu,'Text','Rotate & center slices','MenuSelectedFcn', ...
+uimenu(gui_data.menu.preprocess,'Text','Rotate & center slices','MenuSelectedFcn', ...
     {@ap_histology.rotate_center_slices,histology_toolbar_gui});
-uimenu(preprocess_menu,'Text','Flip & re-order slices','MenuSelectedFcn', ...
+uimenu(gui_data.menu.preprocess,'Text','Flip & re-order slices','MenuSelectedFcn', ...
     {@ap_histology.flip_reorder_slices,histology_toolbar_gui});
 
 % Atlas menu
-atlas_menu = uimenu(histology_toolbar_gui,'Text','Atlas alignment');
-uimenu(atlas_menu,'Text','Choose histology atlas slices','MenuSelectedFcn', ...
+gui_data.menu.atlas = uimenu(histology_toolbar_gui,'Text','Atlas alignment');
+uimenu(gui_data.menu.atlas,'Text','Choose histology atlas slices','MenuSelectedFcn', ...
     {@ap_histology.match_histology_atlas,histology_toolbar_gui});
-uimenu(atlas_menu,'Text','Auto-align histology/atlas slices','MenuSelectedFcn', ...
+uimenu(gui_data.menu.atlas,'Text','Auto-align histology/atlas slices','MenuSelectedFcn', ...
     {@ap_histology.align_auto_histology_atlas,histology_toolbar_gui});
-uimenu(atlas_menu,'Text','Manual align histology/atlas slices','MenuSelectedFcn', ...
+uimenu(gui_data.menu.atlas,'Text','Manual align histology/atlas slices','MenuSelectedFcn', ...
     {@ap_histology.align_manual_histology_atlas,histology_toolbar_gui});
 
 % Annotation menu
-annotation_menu = uimenu(histology_toolbar_gui,'Text','Annotation');
-uimenu(annotation_menu,'Text','Neuropixels probes','MenuSelectedFcn', ...
+gui_data.menu.annotation = uimenu(histology_toolbar_gui,'Text','Annotation');
+uimenu(gui_data.menu.annotation,'Text','Neuropixels probes','MenuSelectedFcn', ...
     {@ap_histology.annotate_neuropixels,histology_toolbar_gui});
 
 % View menu
-view_menu = uimenu(histology_toolbar_gui,'Text','View');
-uimenu(view_menu,'Text','View aligned histology','MenuSelectedFcn', ...
+gui_data.menu.view = uimenu(histology_toolbar_gui,'Text','View');
+uimenu(gui_data.menu.view,'Text','View aligned histology','MenuSelectedFcn', ...
     {@ap_histology.view_aligned_histology,histology_toolbar_gui});
 
 % Create GUI variables
@@ -63,7 +63,7 @@ gui_data.save_path = char;
 guidata(histology_toolbar_gui,gui_data);
 
 % Update GUI text
-update_text(histology_toolbar_gui);
+update_gui(histology_toolbar_gui);
 
 end
 
@@ -79,7 +79,7 @@ gui_data.image_path = uigetdir([],'Select path with raw images');
 guidata(histology_toolbar_gui,gui_data);
 
 % Update GUI text
-update_text(histology_toolbar_gui);
+update_gui(histology_toolbar_gui);
 
 end
 
@@ -95,30 +95,53 @@ gui_data.save_path = uigetdir([],'Select path to save processing');
 guidata(histology_toolbar_gui,gui_data);
 
 % Update GUI text
-update_text(histology_toolbar_gui);
+update_gui(histology_toolbar_gui);
 
 end
 
 
-function update_text(histology_toolbar_gui)
+function update_gui(histology_toolbar_gui)
 
 % Get guidata
 gui_data = guidata(histology_toolbar_gui);
 
+% Check for files
+raw_images_present = ~isempty(gui_data.image_path) && ...
+    ~isempty(dir(fullfile(gui_data.image_path,'*.tif')));
+processed_images_present = ~isempty(gui_data.save_path) && ...
+    ~isempty(dir(fullfile(gui_data.save_path,'*.tif')));
+atlas_slices_present = ~isempty(gui_data.save_path) && ...
+    exist(fullfile(gui_data.save_path,'histology_ccf.mat'),'file');
+atlas_alignment_present = ~isempty(gui_data.save_path) && ...
+    exist(fullfile(gui_data.save_path,'atlas2histology_tform.mat'),'file');
+neuropixels_annotations_present = ~isempty(gui_data.save_path) && ...
+    exist(fullfile(gui_data.save_path,'probe_ccf.mat'),'file');
+
+% Enable/disable appropriate menu options
+gui_data.menu.preprocess.Enable = raw_images_present;
+[gui_data.menu.preprocess.Children(2:end).Enable] = deal(processed_images_present);
+
+gui_data.menu.atlas.Enable = processed_images_present;
+[gui_data.menu.atlas.Children(2:end).Enable] = deal(processed_images_present & atlas_slices_present);
+
+gui_data.menu.annotation.Enable = atlas_alignment_present;
+gui_data.menu.view.Enable = atlas_alignment_present;
+
+
 % Set text
-image_path_text = sprintf('Raw image path:       %s',gui_data.image_path);
-save_path_text = sprintf('Processing save path: %s',gui_data.save_path);
+image_path_text = sprintf('Raw image path:       %s',strrep(gui_data.image_path,filesep,repmat(filesep,1,2)));
+save_path_text = sprintf('Processing save path: %s',strrep(gui_data.save_path,filesep,repmat(filesep,1,2)));
 
 % Check for present files
 % (images)
-if ~isempty(gui_data.image_path)
+if raw_images_present
     n_raw_images = length(dir(fullfile(gui_data.image_path,'*.tif')));
 else
     n_raw_images = 0;
 end
 n_raw_images_text = sprintf('Raw images: %d',n_raw_images);
 
-if ~isempty(gui_data.image_path)
+if processed_images_present
     n_processed_images = length(dir(fullfile(gui_data.save_path,'*.tif')));
 else
     n_processed_images = 0;
@@ -126,31 +149,35 @@ end
 n_processed_images_text = sprintf('Processed images: %d',n_processed_images);
 
 % (alignment)
-if exist(fullfile(gui_data.save_path,'histology_ccf.mat'),'file')
+if atlas_slices_present
     histology_atlas_text = 'Histology atlas slices: YES';
 else
     histology_atlas_text = 'Histology atlas slices: NO';
 end
 
-if exist(fullfile(gui_data.save_path,'atlas2histology_tform.mat'),'file')
+if atlas_alignment_present
     alignment_text = 'Histology-atlas alignment: YES';
 else
     alignment_text = 'Histology-atlas alignment: NO';
 end
 
 % (annotations)
-if exist(fullfile(gui_data.save_path,'probe_ccf.mat'),'file')
-    annotations_text = 'Neuropixels probe annotations';
+if neuropixels_annotations_present
+    annotations_text = 'Neuropixels probe annotations: YES';
 else
     annotations_text = '';
 end
-
-gui_text = {image_path_text,save_path_text,' ', ...
-    n_raw_images_text,n_processed_images_text, ' ', ...
-    histology_atlas_text,alignment_text, ...
+gui_text = { ...
+    '\bf --File paths \rm', ...
+    image_path_text,save_path_text,'\newline', ...
+    '\bf --Images \rm', ...
+    n_raw_images_text,n_processed_images_text, '\newline', ...
+    '\bf --Atlas alignment \rm', ...
+    histology_atlas_text,alignment_text, '\newline', ...
+    '\bf --Annotations \rm', ...
     annotations_text};
 
-set(gui_data.gui_text,'String',gui_text(cellfun(@(x) ~isempty(x),gui_text)));
+set(gui_data.gui_text,'String',gui_text(cellfun(@(x) ~isempty(x),gui_text)))
 
 end
 
