5dd06caf6f14bbc6f653d3e3dc3b062c530a164a
jonahpearl <j4jonahp@gmail.com>
2026-09-22 09:30:28 -0400

    Edit conversion to IBL

--- utility_functions/+ap_histology/export_ibl_probe.m
+++ utility_functions/+ap_histology/export_ibl_probe.m
@@ -19,13 +19,16 @@ save_path = fullfile(fileparts(histology_guidata.histology_processing_filename),
 % Fit line through probe points, return [insertion;tip]
 probe_line_fits = ap_histology.fit_probe_line(histology_guidata.histology_processing_filename);
 
-% Convert CCF to IBL coordinates standard: [AP,ML,DV] in meters
+% Convert CCF to IBL coordinates standard: [ML,AP,DV] in microns
 ibl_bregma_ccf = [540, 44, 570];
 probe_line_fits_ibl = cellfun(@(x) ...
-    (x(:,[1,3,2])-ibl_bregma_ccf([1,3,2]))*10/1000, ...
+    (x(:,[3,1,2]) - ibl_bregma_ccf([3,1,2])) * 10, ...
     {probe_line_fits.ccf},'uni',false);
 
 for curr_probe = 1:length(probe_line_fits_ibl)
+
+    probe_line_fits_ibl{curr_probe}(:,2) = -1 * probe_line_fits_ibl{curr_probe}(:,2);
+    probe_line_fits_ibl{curr_probe}(:,3) = -1 * probe_line_fits_ibl{curr_probe}(:,3);
     xyz_picks_struct = struct('xyz_picks', probe_line_fits_ibl{curr_probe});
 
     save_filename = fullfile(save_path,sprintf('probe%02d',curr_probe-1),'xyz_picks.json');
